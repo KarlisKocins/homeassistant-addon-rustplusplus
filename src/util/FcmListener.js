1ee87a72e2ad0508acb15ded32f799203d1ae157
Kārlis Kociņš <kocins36@gmail.com>
2026-02-11 15:20:50 +0200

    Fix duplicated import

--- src/util/FcmListener.js
+++ src/util/FcmListener.js
@@ -28,7 +28,6 @@ const Battlemetrics = require('../structures/Battlemetrics');
 const Constants = require('../util/constants.js');
 const DiscordButtons = require('../discordTools/discordButtons.js');
 const DiscordEmbeds = require('../discordTools/discordEmbeds.js');
-const DiscordMessages = require('../discordTools/discordMessages.js');
 const DiscordTools = require('../discordTools/discordTools.js');
 const InstanceUtils = require('../util/instanceUtils.js');
 const Map = require('../util/map.js');
